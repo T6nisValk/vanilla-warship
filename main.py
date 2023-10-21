a0ccd3e93577233d61a9fd6fb514f6031b0643d0
T6nisValk <131583739+T6nisValk@users.noreply.github.com>
2023-10-21 13:47:24 +0300

    templates, quote

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+from quotes import quotes
+from random import randint
 
 app = Flask(__name__)  # template_folder = "templates" - can change the folder name
 
 
 @app.route("/")
 def hello():
-    name = "Tõnis"
-    tiny_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    return render_template("base.html", name=name, tiny_list=tiny_list)
+    random_number = randint(0, len(quotes) - 1)
+    random = quotes[random_number]
+    return render_template("firstPage.html", quote=random)
+
+
+@app.route("/something")
+def hello2():
+    random_number = randint(0, len(quotes) - 1)
+    random = quotes[random_number]
+    return render_template("secondPage.html", quote=random)
 
 
 if __name__ == "__main__":
